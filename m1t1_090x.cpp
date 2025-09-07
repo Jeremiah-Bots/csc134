7e4063e01337b4518810e18075c1962dbc732f97
JoshuaNoriega4843 <noriegaj4843@student.faytechcc.edu>
2025-09-07 19:00:37 +0000

    Code almost done

---
+++ m1t1_090x.cpp
@@ -0,0 +1,21 @@
+/*
+CSC 134
+M1T1 for the online secitions
+Joshua Noriega 
+9/7/2025
+Program will greet the user
+ */
+#include <iostream>
+using namespace std;
+int main() {
+  // input:start the program
+  // processing: none
+  // output: 0 (no errors)
+  //special FX: Says "Hello" to the user.
+  
+  // Let's say hello
+  cout << "Hello, user!" << endl; 
+
+
+ return 0; // no errors    
+}
