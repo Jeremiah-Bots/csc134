f4aef7b0fd6b9eed612c2e018309a522ef74f523
JoshuaNoriega4843 <noriegaj4843@student.faytechcc.edu>
2025-09-13 05:42:46 +0000

    Debugging

---
+++ M1LAB_134x.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // This program will simulate an apple orchard.
+    //The own's name
+    string name = "Joshua Noriega" ;
+    // number of apples owned
+    int apples = 100 ;
+    // price per apple
+    double pricePerApple = 0.25 ;
+    // calculate the total price of the apples
+    double totalPrice = (double) apples * pricePerApple ;
+
+
+    // print all the information about the orchard
+    // TODO
+    cout << "welcome to " << name;
+    cout << "'s apple orchard." << endl;
+    cout << "We have" << apples;
+    cout << "apples in stock" << endl;
+    cout << "Apples are currently $" << ;
+    cout << totalPrice << "eac." <<endl;
+    //Finale line, to print totalPrice
+    cout << "If you want them all, that will be $" << 
+    totalPrice << endl;
+
+}
