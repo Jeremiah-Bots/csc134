11ba89bb41fdbd11f6902dab240d086e35695e98
JoshuaNoriega4843 <noriegaj4843@student.faytechcc.edu>
2025-09-24 18:24:42 +0000

    M2T1 Started

---
+++ M2T1x.cpp
@@ -0,0 +1,45 @@
+// csc 134
+// M2T1 - Product sales
+// Joshua Noriega
+// 9/24/2025
+// We're going to make the simplest possible
+// "checkout" machine
+#include <iostream>
+using namespace std;
+
+int main()  {
+    
+
+    //Variables
+    string first_name, last_name, full_name; //holds customer name
+    string product = "apples";
+    double cost_each = 0.99;
+    double total_cost;
+
+
+    // Greatings
+    cout << "welcome to our " << product << " store!" << endl;
+    cout << "What's your first name?" ;
+    cin >> first_name;
+    cout << "What's your last name?";
+    cin >> last_name;
+    full_name = first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+
+    // Ask how much they'd like to purchase
+
+    // Calculate total price
+
+    // Give the result
+
+
+    return 0; // No errors
+
+
+
+
+
+
+
+}
