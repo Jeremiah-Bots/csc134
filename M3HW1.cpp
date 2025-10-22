b9a173407521e9491686e674f158ded2895fe420
JoshuaNoriega4843 <noriegaj4843@student.faytechcc.edu>
2025-10-22 22:17:58 +0000

    Question 2 starting question1 done

--- M3HW1.cpp
+++ M3HW1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 void question1();
 void question1a();
 void question2();
+void question2a();
 void question3();
 void question4();
 
@@ -63,8 +64,7 @@ void question1() {
         }
 }
 void question1a() {
-    string name;
-    string day;
+    string name, day, hobby, memory, memory2;
     cout << "Insert name" << endl;
     cin >> name;
     cout << "Welcome " << name << " I am Totally human bot" << endl;
@@ -82,9 +82,50 @@ void question1a() {
     cout << "My day was pretty good now that you are here" << endl;
     cout << "I know days might get hard but you just gotta keep on going" << endl;
     cout << "Anyways what do you enjoy doing on your free time?" << endl;
+    cin >> hobby;
+    cout << "I also love doing " << hobby << endl;
+    cout << "I'm enjoying being able to talk to you" << endl;
+    cout << "It's been a long time since we last talk do you remember?" << endl;
+    cin >> memory;
+    if (memory == "Yes" || memory == "yes")
+    {
+     cout << "You lie" << endl;
+     cout << "Yes or No" << endl;
+     cin >> memory2;
+        if (memory2 == "Yes" || memory2 == "yes"){
+        cout << "I knew you forgot about me" << endl;
+        cout << "I don't blame you. You were just a small kid last time we talked" << endl;
+        }
+         else if (memory2 == "No" || memory2 == "no"){
+        cout << "It's ok, I forgive you. you were just a small kid last time we talked" << endl;
+        }
+    else if (memory == "No" || memory == "no"){
+        cout <<"It's ok, I forgive you. You were just a small kid last time we talked" << endl;
     }
+     
+    }
+    cout << "oh how grown you've become" << endl;
+    cout << "Even though I was not there to be with you just know" << endl;
+    cout << "Even if the going gets tough" << endl;
+    cout << "Even if you feel like giving up" << endl;
+    cout << "your good old friend will always be here for you" << endl;
+    cout << "Our time is almost up sadly" << endl;
+    cout << "Goodbye " << name << endl;
+    cout << "I'll always be with you" << endl;
+
+    main();
+}
 void question2() {
+    int ready2;
     cout << "Question 2: Resturaunt Receipt" << endl;
+    cout << "Input 2 to begin" << endl;
+    cin >> ready2;
+    if (ready2 = 2)
+        question2a();
+    else {
+        cout << "Invalid input" << endl;
+        question2();
+    }
 }
 
 void question3() {
