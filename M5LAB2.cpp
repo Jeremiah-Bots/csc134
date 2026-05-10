21c1d59c1a732deafb92e85f05c8156079832d7c
Jeremiah-Bots <jeremiah.hart02@gmail.com>
2026-05-10 14:43:37 +0000

    M5LAB2 Done

---
+++ M5LAB2.cpp
@@ -0,0 +1,70 @@
+// CSC 134
+// M5LAB2
+// Hart
+// 5/10/2026
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+double getLength();
+double getWidth();
+double getArea(double width, double length);
+void displayData(double length, double width, double area);
+int main()
+{
+  
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length , width, area;     // The rectangle's width     // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+
+   // Get the rectangle's width.
+   width = getWidth();
+   
+   // Get the rectangle's area.
+   area = getArea(width, length);
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//****************************************
+double getLength()
+{
+    double length;
+      cout << "Enter length:"<< endl;
+      cin >> length;
+      return length;
+}
+double getWidth()
+{
+    double width;
+    cout << "Enter Width" << endl;
+    cin >> width;
+    return width;
+}
+double getArea(double width, double length)
+{
+    double area;
+    area = length * width;
+    return area;
+}
+void displayData(double length, double width, double area)
+{
+cout << "Rectangle Data" << endl;
+cout << "Length: " << length << endl;
+cout << "Width: " << width << endl;
+cout << "Area: " << area << endl;
+}
