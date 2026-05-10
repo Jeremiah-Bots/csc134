bdae2a25d19ef26e60c8387cfb9163a02b2bd215
Jeremiah-Bots <jeremiah.hart02@gmail.com>
2026-05-10 15:51:16 +0000

    M6T1 done

---
+++ M6T1.cpp
@@ -0,0 +1,71 @@
+//CSC 134
+//M6T1 - Loops and Arrays
+//Jeremiah Hart
+//5/10/2026
+
+//Do the same thing with while and for loops, for reference.
+
+
+#include <iostream>
+using namespace std;
+
+
+void part1();
+void part2();
+
+// main
+int main() {
+
+   part1();
+   part2();
+
+    return 0;
+}
+
+
+void part1() {
+    // Method one - no arrays
+    cout << "Enter each car passed per day." << endl;
+    cout << "Day 0 = Monday, Day 4 = Friday" << endl;
+    const int SIZE = 5;
+    int count = 0;
+    int car_today;   
+    int car_total = 0; 
+    double car_avg = 0;
+
+    while (count < SIZE) {
+        cout << "Day " << count << ": ";
+        cin >> car_today;
+        car_total += car_today;
+        count++; 
+    }
+    cout << "Total = " << car_total << endl;
+    car_avg = (double) car_total / SIZE;
+    cout << "Average = " << car_avg << endl;
+}
+
+void part2() {
+
+
+    const int SIZE = 5;
+    string days[SIZE] = {"M", "T", "W", "Th", "F"}; 
+    int car[SIZE];
+    int car_total = 0;
+    double car_avg = 0.0;
+
+    for (int i=0; i < SIZE; i++) {
+        cout << "# on " << days[i] << ": ";
+        cin >> car[i];
+    }
+
+    cout << "Day\t|\tCars" << endl;
+    cout << "--------------------"<< endl;
+    for (int i=0; i < SIZE; i++) {
+        cout << days[i] << "\t|\t" << car[i] << endl;
+        car_total += car[i];
+    }
+   
+    car_avg = (double) car_total / SIZE;
+    cout << "Total = " << car_total << endl;
+    cout << "Average = " << car_avg << endl;
+}
