4419b26c4467488f9b6755cca7d1467d6311f676
Jeremiah-Bots <jeremiah.hart02@gmail.com>
2026-05-13 15:44:26 +0000

    M1LAB done

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -4,25 +4,27 @@
 // 5/10/2026
 #include <iostream>
 using namespace std;
-
 int main() {
-// This program will simulate an apple orchard.
-// The owner’s name
-string name = 'Jeremiah'
-// number of apples owned
-int apples = 100
-// price per apple
-double pricePerApple = 0.25
-// calculate the total price of the apples
-double totalPrice = (double) apples * pricePerApple
-// print all the information about the orchard
-cout << “Welcome to “ << name;
-cout << “ ’s apple orchard.” << endl;
-cout << “We have “ << apples;
-cout << “ apples in stock” << endl;
-cout << “Apples are currently $“;
-cout << totalPrice << “ each.” << endl;
-// Final line, to print totalPrice
-cout << "If you want them all, that will be $" <<
-totalPrice << endl;
+    // This program will simulate an apple orchard.
+    //The own's name
+    string name = "Jeremiah Hart" ;
+    // number of apples owned
+    int apples = 100 ;
+    // price per apple
+    double pricePerApple = 0.25 ;
+    // calculate the total price of the apples
+    double totalPrice = (double) apples * pricePerApple ;
+
+
+    // print all the information about the orchard
+    cout << "Welcome to " << name;
+    cout << "'s apple orchard." << endl;
+    cout << "We have " << apples;
+    cout << " apples in stock." << endl;
+    cout << "Apples are currently $";
+    cout << pricePerApple << " each." <<endl;
+    //Finale line, to print totalPrice
+    cout << "If you want them all, that will be $." << 
+    totalPrice << endl;
+
 }
